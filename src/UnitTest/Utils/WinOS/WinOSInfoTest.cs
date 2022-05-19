5f8085130bc4cec6709681b604c2ef9958540f40
Micahh <micahh28@outlook.com>
2022-05-19 21:18:16 +0800

    test

---
+++ src/UnitTest/Utils/WinOS/WinOSInfoTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OneSteps.Utils.WinOS;
+using System;
+
+namespace UnitTest.Utils.WinOS
+{
+    [TestClass]
+    public class WinOSInfoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var s = WinOSInfo.MachineName();
+            Assert.IsNotNull(s);
+        }
+    }
+}
